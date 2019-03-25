12a137c10f31bad6564e7653d1e42a29e618b559
bmelocarvalho <bmelocarvalho@gmail.com>
2019-03-25 10:05:09 -0300

    Clear search on back buttom and improvement search UX

--- app/src/main/java/com/capivas/mybeers/ui/activity/HomeActivity.java
+++ app/src/main/java/com/capivas/mybeers/ui/activity/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.capivas.mybeers.ui.activity;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,17 +11,15 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
-import android.widget.Toast;
 
 import com.capivas.mybeers.R;
 import com.capivas.mybeers.model.Beer;
 import com.capivas.mybeers.retrofit.RetrofitWrapper;
-import com.capivas.mybeers.ui.listener.OnItemClickListener;
 import com.capivas.mybeers.ui.adapter.PaginationRecyclerViewAdapter;
+import com.capivas.mybeers.ui.listener.OnItemClickListener;
 import com.capivas.mybeers.ui.listener.PaginationScrollListener;
 
 import java.util.List;
@@ -29,15 +28,18 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class HomeActivity extends AppCompatActivity{
-    private static final int PAGE_START = 1;
-    private static final int PER_PAGE = 25;
+public class HomeActivity extends AppCompatActivity {
+    private static final int START_PAGE = 1;
+    private static final int MAX_PER_PAGE = 25;
 
     private RecyclerView beersRecyclerView;
     private ProgressBar progressBar;
     private PaginationRecyclerViewAdapter adapter;
+    private SearchView searchView;
+    private View noSearchResultsView;
+
     private boolean isLoading = false;
-    private int currentPage = PAGE_START;
+    private int currentPage = START_PAGE;
     private String currentQuery = null;
     private boolean isLastPage = false;
 
@@ -61,37 +63,20 @@ public class HomeActivity extends AppCompatActivity{
         return true;
     }
 
-    private void configSearchView(Menu menu) {
-        SearchManager searchManager = (SearchManager) getSystemService(this.SEARCH_SERVICE);
-        MenuItem searchMenuItem = menu.findItem(R.id.home_menu_search);
-        SearchView searchView = (SearchView) searchMenuItem.getActionView();
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                invalidateOptionsMenu();
-                processQuery(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String query) {
-                return false;
-            }
-        });
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-    }
-
-    private void processQuery(String query) {
-        adapter.clear();
-        currentQuery = query;
-        currentPage = PAGE_START;
-        loadNextPage();
+    @Override
+    public void onBackPressed() {
+        if(currentQuery == null) {
+            super.onBackPressed();
+        } else {
+            currentQuery = null;
+            resetBeersList();
+        }
     }
 
     private void getFieldReferences() {
         beersRecyclerView = findViewById(R.id.home_beers_list);
         progressBar = findViewById(R.id.home_progress_bar);
+        noSearchResultsView = findViewById(R.id.home_search_fail);
     }
 
     private void configRecyclerView() {
@@ -131,42 +116,28 @@ public class HomeActivity extends AppCompatActivity{
     }
 
     private void sendBeerToDetailsActivity(Beer beer) {
-        Intent toDetails = new Intent(HomeActivity.this, DetailActivity.class);
+        Intent toDetails = new Intent(this, DetailActivity.class);
         toDetails.putExtra("beer", beer);
         startActivity(toDetails);
     }
 
-    private void loadNextPage() {
-        Log.d("loadNextPage", "loadNextPage: " + currentPage);
-        Call<List<Beer>> call;
+    private Call<List<Beer>> getCall() {
         if(currentQuery != null && !currentQuery.isEmpty())
-            call = RetrofitWrapper.getBeerService().list(currentPage, PER_PAGE, currentQuery);
+            return RetrofitWrapper.getBeerService().list(currentPage, MAX_PER_PAGE, currentQuery);
         else
-            call = RetrofitWrapper.getBeerService().list(currentPage, PER_PAGE);
+            return RetrofitWrapper.getBeerService().list(currentPage, MAX_PER_PAGE);
+    }
 
+    private final  void loadNextPage() {
+        Call<List<Beer>> call = getCall();
         call.enqueue(new Callback<List<Beer>>() {
             @Override
             public void onResponse(Call<List<Beer>> call, Response<List<Beer>> response) {
                 List<Beer> beers = response.body();
-
-                if(beers != null) {
-                    if(currentPage == PAGE_START) {
-                        progressBar.setVisibility(View.GONE);
-                    } else {
-                        adapter.removeLoadingFooter();
-                        isLoading = false;
-                    }
-
-                    adapter.addAll(beers);
-                    if (beers.size() == PER_PAGE)
-                        adapter.addLoadingFooter();
-                    else
-                        isLastPage = true;
-                } else {
-                    isLastPage = true;
-                }
-                Log.i("onResponse", "Success requisition");
-                Log.i("onResponse", "Beers received: " + String.valueOf(beers.size()));
+                updateRecyclerView(beers);
+                Log.i("onResponse",
+                        "Success requisition.Beers received: " + String.valueOf(beers.size())
+                            + "; search query: " + currentQuery);
             }
 
             @Override
@@ -174,5 +145,65 @@ public class HomeActivity extends AppCompatActivity{
                 Log.e("onFailure", "Requisition failure");
             }
         });
+    }
+
+    private void updateRecyclerView(List<Beer> beers) {
+        if(beers != null) {
+            if(beers.size() == 0 && currentQuery != null) {
+                progressBar.setVisibility(View.GONE);
+                noSearchResultsView.setVisibility(View.VISIBLE);
+            } else {
+                noSearchResultsView.setVisibility(View.GONE);
+                addNewBeersToList(beers);
+            }
+        } else {
+            isLastPage = true;
+        }
+    }
+
+    private void addNewBeersToList(List<Beer> beers) {
+        if(currentPage == START_PAGE) {
+            progressBar.setVisibility(View.GONE);
+        } else {
+            adapter.removeLoadingFooter();
+            isLoading = false;
+        }
+
+        adapter.addAll(beers);
+        if (beers.size() < MAX_PER_PAGE) {
+            isLastPage = true;
+        } else {
+            adapter.addLoadingFooter();
+        }
+    }
+
+    private void configSearchView(Menu menu) {
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.home_menu_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.e("SEARCH", "submited!!!");
+                if(!query.isEmpty()) {
+                    currentQuery = query;
+                    resetBeersList();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void resetBeersList() {
+        adapter.clear();
+        currentPage = START_PAGE;
+        isLastPage = false;
+        searchView.onActionViewCollapsed();
+        loadNextPage();
     }
 }
